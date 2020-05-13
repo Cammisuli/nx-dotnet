6645f46f7cf8d68e2bcfb312fa28f599c372cab5
Jonathan Cammisuli <jon@cammisuli.ca>
2020-05-13 11:27:19 -0400

    add project graph dependencies

--- libs/dotnet/src/project-graph.ts
+++ libs/dotnet/src/project-graph.ts
@@ -2,7 +2,13 @@ import {
   ProjectGraphContext,
   ProjectGraphNodeRecords,
   AddProjectDependency,
+  DependencyType,
 } from '@nrwl/workspace';
+
+import { execSync } from 'child_process';
+import { getSortedProjectNodes } from '@nrwl/workspace/src/core/project-graph';
+import { appRootPath } from '@nrwl/workspace/src/utils/app-root';
+import { normalize, resolve } from 'path';
 
 export default function (
   ctx: ProjectGraphContext,
@@ -10,5 +16,49 @@ export default function (
   addDependency: AddProjectDependency,
   fileRead: (s: string) => string
 ) {
-  console.log('Hello from a project graph!');
+  // sort nodes
+  const sortedNodes = getSortedProjectNodes(nodes);
+  // find projects that have a csproj or sln file
+
+  Object.keys(ctx.fileMap).forEach((source) => {
+    const csProj = ctx.fileMap[source].some(
+      (file) => file.ext === '.csproj' || file.ext === '.sln'
+    );
+
+    if (!csProj) {
+      return;
+    }
+
+    const rootPath = nodes[source].data.root;
+
+    /**
+     * Need a better way to get output from this:
+        
+        
+        Project reference(s)
+        --------------------
+        ..\..\libs\dotnet-lib\dotnet-lib.csproj
+
+    */
+    const output = execSync(`dotnet list ${rootPath} reference`)
+      .toString('utf-8')
+      .split('\n');
+    output.splice(0, 2);
+
+    // add deps
+    output.forEach((depPath) => {
+      const depRootPath = normalize(
+        resolve(rootPath, depPath.replace(/\\/g, '/'))
+      ).replace(appRootPath + '/', '');
+      const foundDep = sortedNodes.find((node) => {
+        return depRootPath.startsWith(node.data.root);
+      });
+
+      if (foundDep.name === source) return;
+
+      if (foundDep) {
+        addDependency(DependencyType.static, source, foundDep.name);
+      }
+    });
+  });
 }
